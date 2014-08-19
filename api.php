efc9b91c003ae33bb77eb93ca60d106bb9d98dd8
Tim Hinz <timhinz16@gmail.com>
2014-08-19 00:08:20 -0500

    API Bug fix for posting

--- api.php
+++ api.php
@@ -3,6 +3,6 @@
 	$title=$_POST['title'];
 	$date=date("Y-m-d");
 	$text=$_POST['text'];
-	mysql_query("INSERT INTO `blog`.`blogs` (`ID`, `TITLE`, `DATE`, `TEXT`) VALUES (NULL, '$title', '$date', '$text');");
+	mysql_query("INSERT INTO `blogs` (`ID`, `TITLE`, `DATE`, `TEXT`) VALUES (NULL, '$title', '$date', '$text');");
 	header("location: index.php?remarks=success");
 ?>
